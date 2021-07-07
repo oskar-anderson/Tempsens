f72ee8b0cdc573a017c43e91bf54637856d9aa76
kaande <kaande@ttu.ee>
2021-07-07 15:59:08 +0300

    RNG length

--- site/util/Base64.php
+++ site/util/Base64.php
@@ -7,11 +7,12 @@ require_once (__DIR__."/../../vendor/autoload.php");
 
 class Base64
 {
-   public static function GenerateId(): string
+   public static function GenerateId(int $length = 22): string
    {
-      $guid = base64_encode(random_bytes(40));  // can be lower, not sure about the math, but actual length is about 30% longer after base64_encode
-      $guid = str_replace(['+', '/', '='], ['-', '_', ''], $guid);  // make url friendly
-      return substr($guid, 0, 22);
+      $byteToBase64RatioLength = intval(ceil($length * 3 / 4));
+      $guid = base64_encode(random_bytes($byteToBase64RatioLength));
+      $guid = str_replace(['+', '/'], ['-', '_'], $guid);  // make url friendly
+      return substr($guid, 0, $length); // remove last 0-3 chars if length is not 4 divisible
    }
 
 }
