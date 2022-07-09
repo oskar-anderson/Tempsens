0002f19daf00962c38faf68f3f47198f9cc0e915
oskar-anderson <andersonkarloskar@gmail.com>
2022-07-10 00:28:20 +0300

    _

--- index.php
+++ index.php
@@ -4,9 +4,17 @@
 require 'vendor/autoload.php';
 use \App\util\Config;
 
-echo "test<br>";
-echo "GetConnectUrl: " . (new Config())->GetConnectUrl() . '<br>';
-echo "GetUsername: " . (new Config())->GetUsername() . '<br>';
-echo "getenv: " . getenv("test") . '<br>';
+$line = $_GET['line'];
+echo "test 2<br>";
+if ($line == 1) {
+   echo "GetConnectUrl: " . (new Config())->GetConnectUrl() . '<br>';
+} elseif ($line == 2) {
+   echo "GetUsername: " . (new Config())->GetUsername() . '<br>';
+} elseif ($line == 3) {
+   echo "getenv: " . getenv("test") . '<br>';
+}
+
+
+
 // this kinda works, but messes relative paths up
 // include_once "site/viewController/index.php";
