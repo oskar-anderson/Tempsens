83c75b17752b67f19ac5bc0721ac5e8f4017274f
oskar-anderson <andersonkarloskar@gmail.com>
2022-07-10 13:42:23 +0300

    fix relative paths

--- index.php
+++ index.php
@@ -4,5 +4,7 @@
 require 'vendor/autoload.php';
 
 
-// this kinda works, but messes relative paths up
-include_once "site/viewController/index.php";
+// echo $_SERVER["REQUEST_URI"];
+// Not sure which is better. header allow us to have 1 URL entrypoint, but makes the url longer and uglier
+header('Location: '. "site/viewController/index.php");
+// include_once "site/viewController/index.php";
